e52f5b0808dbd6cc754cfe6f1aadcbfdd43edd3b
Oscar Quest <oscaq@icloud.com>
2026-04-24 22:17:53 +0100

    Prices

--- trader_phase_2.py
+++ trader_phase_2.py
@@ -12,6 +12,25 @@ class Trader:
     def trade_vev(self, state: TradingState, product: str, product_position: int) -> List[Order]:
         orders = []
 
+        order_depth = state.order_depths[product]
+
+        best_bid = max(order_depth.buy_orders.keys())
+        best_ask = min(order_depth.sell_orders.keys())
+        mid_price = (best_bid + best_ask) / 2
+
+        ## ARBITRAGE ##
+        for price, quantity in sorted(state.order_depths[product].sell_orders.items()):
+            if price < mid_price and product_position < 200:
+                buy_quantity = min(200 - product_position, -quantity)
+                orders.append(Order(product, price, buy_quantity))
+                product_position += buy_quantity
+
+        for price, quantity in sorted(state.order_depths[product].buy_orders.items(), reverse=True):
+            if price > mid_price and product_position > -200:
+                sell_quantity = min(200 + product_position, quantity)
+                orders.append(Order(product, price, -sell_quantity))
+                product_position -= sell_quantity
+
         ## CODE HERE ##
 
         return orders
